979504466a48dd3eaaf771062fe173c9fb6a8aa2
Guilherme Zurid Meneses Costa <gzmcosta@ipes.se.gov.br>
2024-03-11 10:28:44 -0300

    Interpolação de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
-
+  name:string = 'Guilherme';
+  constructor(){
+    
+  }
 }
