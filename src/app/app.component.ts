8af7a1106b6ebb8385a177ef1599e5f604ecf4ae
Guilherme Zurid Meneses Costa <gzmcosta@ipes.se.gov.br>
2024-03-11 10:41:22 -0300

    Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = 'Joaquin'
+  idade = 30
   title = 'angular-estrutura';
 }
