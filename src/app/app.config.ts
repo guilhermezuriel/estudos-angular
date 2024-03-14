ce0ffbac89d0cfa202dcfb427e4bd5e6da9ba6d1
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-14 08:27:10 -0300

    Aplicando router-outlet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideProtractorTestingSupport} from '@angular/platform-browser'
+import { routeConfig } from '../routes';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [
+    provideProtractorTestingSupport(),
+    provideRouter(routeConfig)
+  ]
+}
